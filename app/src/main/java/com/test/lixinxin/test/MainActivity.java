ef1b4082c13c500406232faae8beafb6ba489458
skylixinxin <xinnianwei@qq.com>
2015-06-02 18:26:43 +0800

    liu

--- app/src/main/java/com/test/lixinxin/test/MainActivity.java
+++ app/src/main/java/com/test/lixinxin/test/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
     public TextView textview01;
     private RadioButton radioButton;
     private Spinner spinner;
-    private TextView textView02;
+    private TextView textview03;
 
 
     @Override
