5e32c7478b2c7419d194913b1da2c008c7a55fa9
skylixinxin <xinnianwei@qq.com>
2015-06-02 14:45:46 +0800

    study test second

--- app/src/main/java/com/test/lixinxin/test/MainActivity.java
+++ app/src/main/java/com/test/lixinxin/test/MainActivity.java
@@ -4,13 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView textview01;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
