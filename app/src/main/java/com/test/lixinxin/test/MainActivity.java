ad9bb069a0d33b30bf6da5127326d119e23bb70c
skylixinxin <xinnianwei@qq.com>
2015-06-02 15:49:14 +0800

    branch exercise

--- app/src/main/java/com/test/lixinxin/test/MainActivity.java
+++ app/src/main/java/com/test/lixinxin/test/MainActivity.java
@@ -8,7 +8,8 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    public TextView textview01;
+    private TextView textview01;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
